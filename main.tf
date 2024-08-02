13902ed15657048620663d02b5a08a780e419e56
UPARAG NATH <uparag13@gmail.com>
2024-08-02 23:53:17 +0530

    main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "eu-north-1c"  
+}
+
+resource "aws_instance" "foo" {
+  ami           = "ami-07c8c1b18ca66bb07"
+  instance_type = "t2.micro"
+  tags = {
+      Name = "TF-Instance"
+  }
+}
